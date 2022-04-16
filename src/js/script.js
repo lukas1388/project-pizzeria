ec2e125f95cf3444fae76ce21b4d162098dad743
Łukasz Staniszewski <lukas13.88@gmail.com>
2022-04-16 20:21:54 +0200

    Modified method processOrder, refactoring in script.js file

--- src/js/script.js
+++ src/js/script.js
@@ -85,6 +85,7 @@
       thisProduct.formInputs = thisProduct.element.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -141,8 +142,19 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           // console.log(optionId, option);
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)){
+          if(optionSelected){
             // check if the option is not default
             if(!option.default){
               // add option price to price variable
